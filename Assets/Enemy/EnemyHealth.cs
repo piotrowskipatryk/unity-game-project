781e79bc8ffe8f992532c71eb1a62a82de8045d8
marwiasz <132089254+marwiasz@users.noreply.github.com>
2023-05-17 15:43:49 +0200

    Improved pathfinding & enemy targeting

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -6,7 +6,8 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
     [SerializeField] int currentHitPoints = 0;
-    void Start()
+
+    void OnEnable()
     {
         currentHitPoints = maxHitPoints;
     }
@@ -21,7 +22,7 @@ public class EnemyHealth : MonoBehaviour
 
         if(currentHitPoints <= 0)
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false); 
         }
     }
 
