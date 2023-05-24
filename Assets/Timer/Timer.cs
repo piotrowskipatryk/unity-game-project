91fce20cdb581c649e152412034a1356da995b5d
marwiasz <132089254+marwiasz@users.noreply.github.com>
2023-05-24 23:05:48 +0200

    Timer & loading next level

---
+++ Assets/Timer/Timer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeLeft = 200f;
+    bool timerOn = false;
+
+    public TextMeshProUGUI timerText;
+
+    void Start()
+    {
+        timerOn = true;
+    }
+
+    void Update()
+    {
+        if(timerOn)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                UpdateTimer(timeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                timeLeft = 0;
+                timerOn = false;
+                LoadNextLevel();
+            }
+        }
+    }
+
+    void UpdateTimer(float currentTime)
+    {
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = "Time remaining: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+}
